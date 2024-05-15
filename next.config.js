84f6e1f036ff1c7f8c0d047a25542cabe5af23b3
Linbo <121047427+l6cao@users.noreply.github.com>
2024-05-15 02:07:42 -0400

    Update next.config.js to fix GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  basePath: isProd ? '/l6cao.github.io' : '',
-  assetPrefix: isProd ? '/l6cao.github.io/' : '',
+  basePath: isProd ? '' : '',
+  assetPrefix: isProd ? '/' : '',
   images: {
     unoptimized: true,
   },
