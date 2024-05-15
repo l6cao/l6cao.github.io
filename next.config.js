bbc00475817d69c0226e5ccd339ad0ef3ea6152b
Linbo <121047427+l6cao@users.noreply.github.com>
2024-05-15 02:36:12 -0400

    Update next.config.js for static export

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   basePath: isProd ? '' : '',
   assetPrefix: isProd ? '/' : '',
+  output: 'export',
   images: {
     unoptimized: true,
   },
