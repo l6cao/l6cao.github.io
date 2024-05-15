8772d5aff7de3aac0756182032a72a0759ab279f
Linbo <121047427+l6cao@users.noreply.github.com>
2024-05-15 02:16:52 -0400

    Update configuration for static export and GitHub Pages

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
   basePath: isProd ? '/l6cao.github.io' : '',
   assetPrefix: isProd ? '/l6cao.github.io/' : '',
-  trailingSlash: true,
+  output: 'export',
   images: {
     unoptimized: true,
   },
