4d565ca8ab9539e805fab40c53ab2009487e38b8
Linbo <121047427+l6cao@users.noreply.github.com>
2024-05-15 02:50:22 -0400

    Updates

--- _next/static/fhMvA28ijMK662wl6r8Xv/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/65aa3619a1925c9d.css","static/chunks/547-fc67c1a53924a787.js","static/chunks/pages/index-124038cfe838a4d4.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
