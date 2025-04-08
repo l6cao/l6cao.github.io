1bda77f1473756530f5b3c835173ac892756a6f5
Linbo <121047427+l6cao@users.noreply.github.com>
2025-04-08 09:01:53 -0400

    Add redirect to personal.035100.xyz

--- components/Header.js
+++
@@ -1,8 +0,0 @@
-// components/Header.js
-export default function Header() {
-  return (
-    <header className="bg-blue-600 p-4 text-white">
-      <h1 className="text-3xl">My Cool Website</h1>
-    </header>
-  );
-}
