8bb5db6e440454fe56b527014e800eeb946fefbc
Linbo <121047427+l6cao@users.noreply.github.com>
2025-04-08 15:48:29 -0400

    April_8

--- pages/index.js
+++ pages/index.js
@@ -75,7 +75,7 @@ export default function Home() {
               <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold font-title tracking-tighter text-surface">
                 Linbo Cao
                 <br />
-                <span className="text-accent-green font-accent whitespace-normal md:whitespace-nowrap">AI Researcher</span>
+                <span className="text-accent-green font-accent whitespace-normal md:whitespace-nowrap">LLM Researcher</span>
                 <br className="block" />
                 <span className="block">&amp; Developer</span>
               </h1>
